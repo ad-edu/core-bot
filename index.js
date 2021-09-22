4dfdbfceac4b208177320a9791aed3ce43f21efc
ad-edu <arijit.dey.edu@outlook.com>
2021-09-22 19:07:22 +0530

    hello

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ const bot = new DialogAndWelcomeBot(conversationState, userState, dialog);
 
 // Create HTTP server
 const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function() {
+server.listen(process.env.port || process.env.PORT || 80, function() {
     console.log(`\n${ server.name } listening to ${ server.url }`);
     console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
     console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
